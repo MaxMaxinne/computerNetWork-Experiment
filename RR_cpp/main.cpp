6e6c7e4efc0a55d495adc658eb412de653752d61
MaxMaxinne <1036815812@qq.com>
2020-11-09 22:23:00 +0800

    add throughout

--- RR_cpp/main.cpp
+++ RR_cpp/main.cpp
@@ -2,8 +2,8 @@
 
 int main(int argc,char** argv){
     producer p[PROD_NUM];
-    double lamda[PROD_NUM]={atof(argv[2]),atof(argv[3]),atof(argv[4])};
-    double Mu[PROD_NUM]={atof(argv[5]),atof(argv[6]),atof(argv[7])};
+    double lamda[PROD_NUM]={atof(argv[1]),atof(argv[2]),atof(argv[3])};
+    double Mu[PROD_NUM]={atof(argv[4]),atof(argv[5]),atof(argv[6])};
     cout<<"M/M/"<<PROD_NUM<<" queueLen: "<<QUEUE_LEN<<endl;
     for(int i=0;i<PROD_NUM;i++)
         p[i]=producer(i,lamda[i],Mu[i]);
